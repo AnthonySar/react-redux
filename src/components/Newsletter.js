105f6fe7bca239523b4ed29d0b4abd8b30dcf77e
AnthonySar <anthony.sarapata@gmail.com>
2023-04-19 14:50:16 +0200

    :wrench: jest & testing library en cours

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -45,12 +45,13 @@ const Newsletter = () => {
 
   return (
     <div className='formikContainer'>
-      <h2>Test de Formik</h2>
+      <h2 data-testid='heading-test'>Test de Formik</h2>
 
       <form onSubmit={formik.handleSubmit}>
         <div className='formikGroup'>
           <label htmlFor='firstName'>Prénom</label>
           <input 
+            data-testid='input-firstname'
             id='firstName'
             name='firstName'
             type='text'
@@ -85,6 +86,15 @@ const Newsletter = () => {
             value={formik.values.email}
           />
           { formik.touched.email && formik.errors.email ? <span>{formik.errors.email}</span> : null }
+        </div>
+
+        <div id='checklist'>
+          <input value="1" name="r" type="checkbox" id="01" />
+          <label htmlFor="01">1er Checkbox</label>
+          <input value="2" name="r" type="checkbox" id="02" />
+          <label htmlFor="02">2ième Checkbox</label>
+          <input value="3" name="r" type="checkbox" id="03" />
+          <label htmlFor="03">3ième Checkbox</label>
         </div>
 
         <button data-testid="button-submit" type='submit'>Valider</button>
--- src/components/Newsletter.test.js
+++ src/components/Newsletter.test.js
@@ -2,8 +2,9 @@ import { render, screen } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
 import Newsletter from './Newsletter';
 
-it('render', () => {
+it('render', async () => {
   render(<Newsletter />);
 
   expect(screen.getByTestId('button-submit')).toBeInTheDocument();
+  expect(screen.getByTestId('heading-test')).toHaveTextContent('Test de Formik');
 })
