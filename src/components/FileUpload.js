71fa082f98339cdce26caef04f5242f410bc0684
AnthonySar <anthony.sarapata@gmail.com>
2023-04-19 16:15:57 +0200

    :wrench: Commentaire pour le componant FileUpload

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,6 +1,19 @@
 import { useField } from 'formik'
 import React from 'react'
 
+/**
+ * @param useField 
+ * @description
+ * useField : est utilisée pour associer le champ 'name' du formulaire à l'input.
+ * Permet de gérer les états de validations et les valeurs de champs 
+ */
+
+/**
+ * @param fileRef
+ * @description
+ * La référence est utilisé pour récupérer les fichiers sélectionnés dans le composant parent
+ */
+
 const FileUpload = ({ fileRef, ...props }) => {
   const [field, meta] = useField(props)
 
